caff767f156d2968c56383f23a9cf278ed8d0369
omarzabin <omarszabin@gmail.com>
2022-04-11 00:26:38 +0300

    first

---
+++ app/src/main/java/com/example/housekeeping/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.housekeeping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+        }
+    }
+}
